3c3ceb024ec3db8f8ab079f3a879d0b87d239043
Owen Rowell <owen.rowell@zoho.com>
2021-10-08 15:27:43 +0100

    Added basic structure

---
+++ code/simulation.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+class Simulation:
+    def __init__(self):
+        pass
+    
+    def run_sim(self):
+        pass
+        
+    def step(self):
+        pass
+    
+    def animate(self):
+        pass
+    
+if __name__ == '__main__':
+    pass
