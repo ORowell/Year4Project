0660b63b1fca8a3d47a75b9e1f112cb50d248276
Owen Rowell <owen.rowell@zoho.com>
2021-10-14 16:37:14 +0100

    Added forces and animation

--- code/simulation.py
+++ code/simulation.py
@@ -1,10 +1,14 @@
-from typing import Tuple
 import numpy as np
+from scipy.special import kn
+import tqdm
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
 
+BAR_FORMAT = '{desc}: {percentage:5.1f}%|{bar}{r_bar}'
 
 class Simulation:
     def __init__(self, x_size, y_size, x_repeats, y_repeats):
-        self.vortices: np.ndarray = np.empty(shape=(0,2))
+        self.vortices: np.ndarray = np.empty(shape=(0, 2))
         
         self.x_size = x_size
         self.y_size = y_size
@@ -23,20 +27,30 @@ class Simulation:
         
         self.vortices = np.append(x_vals, y_vals, axis=1)
     
-    def run_sim(self):
-        pass
+    def run_sim(self, total_time: float, dt: float):
+        num_steps = int(total_time/dt)
+        self.result = np.empty((num_steps+1, *self.vortices.shape))
+        self.result[0] = self.vortices.copy()
+        
+        print('Running simulation')
+        for i in tqdm.tqdm(range(num_steps), desc='Simulating', bar_format=BAR_FORMAT):
+            self.step(dt)
+            self.result[i] = self.vortices.copy()
+            
+        return self.result            
         
     def step(self, dt: float):
         images = self.get_images()
+        all_vortices = np.concatenate((self.vortices, images))
         new_vortices = self.vortices.copy()
         for i, vortex in enumerate(self.vortices):
-            other_vortices = np.delete(self.vortices, i)
-            all_acting_vortices = np.concatenate((other_vortices, images))
-            force = self.calculate_force(vortex, all_acting_vortices)
+            acting_vortices = np.delete(all_vortices, i, axis=0)
+            force = self.calculate_force(vortex, acting_vortices)
             
             new_vortices[i] += dt*force
         
         self.vortices = new_vortices
+        self.wrap_particles()
     
     def get_images(self):
         x_offset = np.repeat(np.arange(-self.x_images, self.x_images+1), 2*self.y_images+1) * self.x_size
@@ -53,7 +67,14 @@ class Simulation:
         return np.stack((x_images, y_images), axis=1)
     
     def calculate_force(self, vortex_pos, other_pos):
-        pass
+        rel_pos = vortex_pos - other_pos
+        distances = np.linalg.norm(rel_pos, axis=1, keepdims=True)
+        directions = rel_pos/distances
+        
+        force_sizes = kn(1, distances)
+        forces = force_sizes * directions
+        
+        return np.sum(forces, axis=0)
     
     def wrap_particles(self):
         x_num_shifts = self.vortices[:, 0]//self.x_size
@@ -64,11 +85,48 @@ class Simulation:
         y_shift = np.outer(y_num_shifts, self.y_size_ary)
         self.vortices -= y_shift
     
-    def animate(self):
-        pass
+    def animate(self, filename):
+        n_steps, num_vortices, _ = self.result.shape
+        
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.set_xlim([0, self.x_size])
+        ax.set_ylim([0, self.y_size])
+        
+        self.dots = [ax.plot([], [], 'o', c='r')[0] for i in range(num_vortices)]
+        
+        animator = anim.FuncAnimation(fig, self._anim_update, n_steps, blit = True)
+        
+        self.p_bar = tqdm.tqdm(total = n_steps+1, desc='Animating ', unit='fr', bar_format=BAR_FORMAT)
+        animator.save(f'results\\{filename}', fps=30)
+        self.p_bar.close()
+        
+    def _anim_update(self, frame_num):
+        self.p_bar.update(1)
+        for i, dot in enumerate(self.dots):
+            dot.set_data(self.result[frame_num, i])
+            
+        return self.dots
+    
+def ground_state():
+    sim = Simulation(2, 1, 1, 1)
+    
+    sim.create_vortex(0, 0)
+    sim.create_vortex(1, 0)
+    sim.create_vortex(0.5, 0.5)
+    sim.create_vortex(1.5, 0.5)
+    
+    sim.run_sim(0.2, 0.001)
+    sim.animate('groundstate.gif')
+    
+def many_vortices():
+    sim = Simulation(4, 3, 1, 1)
+    
+    sim.create_random_vortices(150)
+    
+    sim.run_sim(0.4, 0.001)
+    sim.animate('lots.gif')
     
 if __name__ == '__main__':
-    sim = Simulation(2, 3, 1, 1)
-    sim.create_random_vortices(5)
-    
-    sim.get_images()
+    # ground_state()
+    many_vortices()
