16901faeee4d24edfc2d0b46befed23b5e6802b4
Owen Rowell <owen.rowell@zoho.com>
2021-12-07 16:09:48 +0000

    Testing analytic form of potential

--- code/testing.py
+++ code/testing.py
@@ -1,4 +1,5 @@
 import timeit
+from typing import Union
 
 def zero_bessel_time():
     setup = """
@@ -24,5 +25,41 @@ ary = np.random.uniform(9, 20, 1000)
     
     return min(timeit.repeat(test, setup, number=10000))
 
-print(zero_bessel_time())
-print(random_bessel_time())
+# print(zero_bessel_time())
+# print(random_bessel_time())
+
+import numpy as np
+from scipy.special import kn
+A_X = 1
+LAMBDA = 1
+
+def v_l_true(x, y, N=100):
+    total = 0
+    for n in range(-N, N+1):
+        vals = np.sqrt((x + n*A_X)**2 + y**2)/LAMBDA
+        total += kn(0, vals)
+    return total
+
+def v_l_analytic(x, y, N=100):
+    total = 0
+    for n in range(-N, N+1):
+        const = 2*np.pi*n/A_X
+        q_n = np.sqrt(1/LAMBDA**2 + const**2)
+        vals = np.exp(-1j*const*x - q_n*np.abs(y))/q_n
+        total += vals
+    return np.pi/A_X*total
+
+def v_l_fourier_analytic(kx, ky, N=100):
+    total = 0
+    const = 1/LAMBDA**2 + kx**2 + ky**2
+    for n in range(-N, N+1):
+        val = np.exp(-1j*n*A_X*kx)
+        total += val/const
+    return total
+
+    
+n = 10000
+x=np.linspace(0, 15, 100)
+a = v_l_true(x, 1, n)
+b = v_l_analytic(x, 1, n)
+print(a-b)
